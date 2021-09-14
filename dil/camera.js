ac9282e06bb8cf9040da82701bc090ab5ed4d50d
abekazumasa <kazumasa.danmaro@gmail.com>
2021-09-14 13:43:31 +0900

    ok7

--- dil/camera.js
+++ dil/camera.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const image = document.getElementById("image_file");
 const showImage = document.getElementById("viewImage");
 
@@ -14,25 +13,3 @@ image.addEventListener("change", () => {
       reader.readAsDataURL(inputFile);
   }
 });
-=======
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
-window.URL = window.URL || window.webkitURL;
-var video = document.getElementById('myVideo');
-var localStream = null;
-navigator.getUserMedia({video: true, audio: false}, 
- function(stream) { // for success case
-  console.log(stream);
-  var userAngent = window.navigator.userAgent.toLowerCase();
-
-  if(userAngent.indexOf('chrome')!=-1){
-    video.srcObject = stream;
-  }else{
-  video.src = window.URL.createObjectURL(stream);
-  }
-},
- function(err) { // for error case
-  console.log(err);
- }
-);
->>>>>>> remotes/origin/main

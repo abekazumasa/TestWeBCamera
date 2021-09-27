99c6babd999adb05105ba40193c0b0307b5c62f3
abekazumasa <kazumasa.danmaro@gmail.com>
2021-09-27 13:58:45 +0900

    test1

--- dil/printSetting.js
+++ dil/printSetting.js
@@ -10,20 +10,20 @@ $(function () {
         var data = canvasData.toDataURL();
         outputImg.src = data;
         $('.print-area').append(outputImg);    
-
-        setTimeout(function(){
-            print();
-           
-        },200);
-        
-        
-    });
-    function print(){
         //プリントしたいエリアの取得
         //プリント用の要素「#print」を作成し、上で取得したprintAreaをその子要素に入れる。
         $('body').append('<div id="print" class="printBc"></div>');
         $('.print-area').clone().appendTo('#print');
         $('body > :not(#print)').addClass('print-off');
+        setTimeout(function(){
+            print();
+           
+        },100);
+        
+        
+    });
+    function print(){
+
         window.print();
         $('#print').remove();
         $('.print-off').removeClass('print-off');

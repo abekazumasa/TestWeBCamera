29483088a92b69f49a21cf5f7504a7049363ca25
abekazumasa <kazumasa.danmaro@gmail.com>
2021-10-05 14:48:22 +0900

    ok

--- dil/printSetting.js
+++ dil/printSetting.js
@@ -1,18 +1,22 @@
 
-const canvasData = document.getElementById("canvas-area");
+
 $(function () {
-    var outputImg = document.createElement('img');
+
     //印刷ボタンをクリックした時の処理
     $('.print-btn').click(function () {
+        var canvasData = document.getElementById("canvas-area");
         var data = canvasData.toDataURL();
+        var outputImg = document.getElementById("getImgTag");
         outputImg.src = data;
+        outputImg.style.Width = canvasData.style.width;
+        outputImg.style.Height = canvasData.style.Height;
+
         $('.print-area').append(outputImg);
         $('.printwarp').addClass('print');
         $('body > :not(.print)').addClass('print-off');
         window.print();
-        $('print').removeClass('print');
+        $('.print').removeClass('print');
         $('.print-off').removeClass('print-off');
-        outputImg.remove();
-        
+
     });
 });
